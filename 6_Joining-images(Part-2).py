b6d081c534c320b75f0237d071a375b6e5994da9
Ritik <ritikbhola08@gmail.com>
2021-07-31 15:50:24 +0530

    Day 3

---
+++ 6_Joining-images(Part-2).py
@@ -0,0 +1,54 @@
+import cv2 
+import numpy as np
+
+img = cv2.imread("Resources/card2.jpeg")
+"""
+#  In order to perform horizontal and vertical stack we need both images to have same number of kernel i.e they must be both rgb or both must be grey.
+imghor=np.hstack((img,img))  # horizontal Stack
+imgVer=np.vstack((img,img))  # vertical Stack
+
+""" 
+
+# Second Method
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ 
+imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
+cv2.imshow("ImageStack",imgStack)
+
+# cv2.imshow("Horizontal",imghor)
+# cv2.imshow("Vertical",imgVer)
+
+cv2.waitKey(0)
+
